27ce7afe304c1aa6929564fa98c76456a9e26f74
Lucifer <fer.reyes151719@gmail.com>
2020-11-11 21:53:15 -0600

    a

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -1,11 +1,13 @@
-<!-- SCRIPTS -->
-  <!-- JQuery -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
+    <script>
+        $(document).ready(function() {
+        $(".button-collapse").sideNav2();
+        var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+        var ps = new PerfectScrollbar(sideNavScrollbar);
+        });
+    </script>
 </body>
 </html>
--- application/views/template_web/temp/View_header.php
+++ application/views/template_web/temp/View_header.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -7,14 +5,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<base href = "<?php echo base_url(); ?>">
-	<!-- Font Awesome -->
+
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-	<!-- Bootstrap core CSS -->
 	<link href="<?php echo base_url() ?>assets_front/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Material Design Bootstrap -->
 	<link href="<?php echo base_url() ?>assets_front/css/mdb.min.css" rel="stylesheet">
-	<!-- Your custom styles (optional) -->
 	<link href="<?php echo base_url() ?>assets_front/css/style.css" rel="stylesheet">
 </head>
 <body>
->>>>>>> 5d2b88b9605250b248d390461862e9947b32529c
+
