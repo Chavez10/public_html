e3ab4a9c5568c2b4c91b3d2728cd552ecbed1bf1
Lucifer <fer.reyes151719@gmail.com>
2020-11-11 22:01:35 -0600

    a

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -2,12 +2,5 @@
     <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
-    <script>
-        $(document).ready(function() {
-        $(".button-collapse").sideNav2();
-        var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-        var ps = new PerfectScrollbar(sideNavScrollbar);
-        });
-    </script>
 </body>
 </html>
--- application/views/template_web/temp/prueba.php
+++ application/views/template_web/temp/prueba.php
@@ -1,98 +1,15 @@
-<!-- SideNav slide-out button -->
-<a href="#" data-activates="slide-out" class="btn btn-primary p-3 button-collapse"><i
-    class="fas fa-bars"></i></a>
-
-<!-- Sidebar navigation -->
-<div id="slide-out" class="side-nav2 side">
-  <ul class="custom-scrollbar">
-    <!-- Logo -->
-    <li>
-      <div class="logo-wrapper waves-light">
-        <a href="#"><img src="https://mdbootstrap.com/img/logo/mdb-transparent.png"
-            class="img-fluid flex-center"></a>
-      </div>
-    </li>
-    <!--/. Logo -->
-    <!--Social-->
-    <li>
-      <ul class="social">
-        <li><a href="#" class="icons-sm fb-ic"><i class="fab fa-facebook-f"> </i></a></li>
-        <li><a href="#" class="icons-sm pin-ic"><i class="fab fa-pinterest"> </i></a></li>
-        <li><a href="#" class="icons-sm gplus-ic"><i class="fab fa-google-plus-g"> </i></a></li>
-        <li><a href="#" class="icons-sm tw-ic"><i class="fab fa-twitter"> </i></a></li>
-      </ul>
-    </li>
-    <!--/Social-->
-    <!--Search Form-->
-    <li>
-      <form class="search-form" role="search">
-        <div class="form-group md-form mt-0 pt-1 waves-light">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-      </form>
-    </li>
-    <!--/.Search Form-->
-    <!-- Side navigation links -->
-    <li>
-      <ul class="collapsible collapsible-accordion">
-        <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-chevron-right"></i> Submit
-            blog<i class="fas fa-angle-down rotate-icon"></i></a>
-          <div class="collapsible-body">
-            <ul>
-              <li><a href="#" class="waves-effect">Submit listing</a>
-              </li>
-              <li><a href="#" class="waves-effect">Registration form</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-hand-pointer"></i>
-            Instruction<i class="fas fa-angle-down rotate-icon"></i></a>
-          <div class="collapsible-body">
-            <ul>
-              <li><a href="#" class="waves-effect">For bloggers</a>
-              </li>
-              <li><a href="#" class="waves-effect">For authors</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li><a class="collapsible-header waves-effect arrow-r"><i class="fas fa-eye"></i> About<i
-              class="fas fa-angle-down rotate-icon"></i></a>
-          <div class="collapsible-body">
-            <ul>
-              <li><a href="#" class="waves-effect">Introduction</a>
-              </li>
-              <li><a href="#" class="waves-effect">Monthly meetings</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li><a class="collapsible-header waves-effect arrow-r"><i class="far fa-envelope"></i> Contact me<i
-              class="fas fa-angle-down rotate-icon"></i></a>
-          <div class="collapsible-body">
-            <ul>
-              <li><a href="#" class="waves-effect">FAQ</a>
-              </li>
-              <li><a href="#" class="waves-effect">Write a message</a>
-              </li>
-              <li><a href="#" class="waves-effect">FAQ</a>
-              </li>
-              <li><a href="#" class="waves-effect">Write a message</a>
-              </li>
-              <li><a href="#" class="waves-effect">FAQ</a>
-              </li>
-              <li><a href="#" class="waves-effect">Write a message</a>
-              </li>
-              <li><a href="#" class="waves-effect">FAQ</a>
-              </li>
-              <li><a href="#" class="waves-effect">Write a message</a>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-    </li>
-  </ul>
-  <div class="sidenav-bg rgba-blue-strong"></div>
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h4 class="text-white">Collapsed content</h4>
+      <span class="text-muted">Toggleable via the navbar brand.</span>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+      data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
 </div>
