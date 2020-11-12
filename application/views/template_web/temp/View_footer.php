3c1cc0f112b40d9a839b9bc0a664829faa5f8407
Lucifer <fer.reyes151719@gmail.com>
2020-11-11 22:04:08 -0600

    a

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -1,6 +1,11 @@
-	<script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
+<!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
 </body>
 </html>
