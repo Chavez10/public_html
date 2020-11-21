2a0b33f448d152d06aad63203154788c7e65947c
Lucifer <fer.reyes151719@gmail.com>
2020-11-20 18:55:07 -0600

    a

--- application/views/template_web/temp/prueba.php
+++ application/views/template_web/temp/prueba.php
@@ -8,8 +8,8 @@
   <div class="pos-f-t">
   <div class="collapse" id="navbarToggleExternalContent">
     <div class="bg-dark p-4">
-      <h4 class="text-white">Collapsed content</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
+      <h4 class="text-white">Gilbertp</h4>
+      <span class="text-muted">Se la come.</span>
     </div>
   </div>
 </div>
