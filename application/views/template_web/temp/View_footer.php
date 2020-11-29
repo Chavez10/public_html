5ab37643f18bd93b96ace0e14f32fefc04f30c01
Gilberto98 <gilberto.ponce14@gmail.com>
2020-11-29 17:12:03 -0600

    cambio

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -1,11 +1,11 @@
-<!-- SCRIPTS -->
-  <!-- JQuery -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="<?php echo base_url() ?>assets_front/js/mdb.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/js-cookie/js.cookie.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.extension.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/js/argon.js?v=1.2.0"></script>
+
 </body>
 </html>
--- application/views/template_web/temp/View_header.php
+++ application/views/template_web/temp/View_header.php
@@ -6,10 +6,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<base href = "<?php echo base_url(); ?>">
 
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-	<link href="<?php echo base_url() ?>assets_front/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php echo base_url() ?>assets_front/css/mdb.min.css" rel="stylesheet">
-	<link href="<?php echo base_url() ?>assets_front/css/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+	<link href="<?php echo base_url() ?>assets_F/img/brand/favicon.png" type="image/png">
+	<link href="<?php echo base_url() ?>assets_F/vendor/nucleo/css/nucleo.css" type="text/css">
+	<link href="<?php echo base_url() ?>assets_F/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+	<link href="<?php echo base_url() ?>assets_F/css/argon.css?v=1.2.0" type="text/css">
+	
+
 </head>
 <body>
 
