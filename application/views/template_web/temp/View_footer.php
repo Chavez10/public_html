00708e9824a5209f844d428c60c61231b225654a
Gilberto98 <gilberto.ponce14@gmail.com>
2020-11-29 18:16:26 -0600

    Assets2

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/js-cookie/js.cookie.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery.scrollbar/jquery.scrollbar.min.js."></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.extension.js"></script>
