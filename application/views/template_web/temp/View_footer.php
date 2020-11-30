6410ab76280e9cfb931ec947af9c2c8fe43876f8
Gilberto98 <gilberto.ponce14@gmail.com>
2020-11-29 18:19:36 -0600

    c

--- application/views/template_web/temp/View_footer.php
+++ application/views/template_web/temp/View_footer.php
@@ -1,11 +1,4 @@
 <script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/js-cookie/js.cookie.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery.scrollbar/jquery.scrollbar.min.js."></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/vendor/chart.js/dist/Chart.extension.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets_F/js/argon.js?v=1.2.0"></script>
 
 </body>
 </html>
--- application/views/template_web/temp/View_header.php
+++ application/views/template_web/temp/View_header.php
@@ -7,11 +7,7 @@
 	<base href = "<?php echo base_url(); ?>">
 
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-	<link href="<?php echo base_url() ?>assets_F/img/brand/favicon.png" type="image/png">
-	<link href="<?php echo base_url() ?>assets_F/vendor/nucleo/css/nucleo.css" type="text/css">
-	<link href="<?php echo base_url() ?>assets_F/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-	<link href="<?php echo base_url() ?>assets_F/css/argon.css?v=1.2.0" type="text/css">
-	
+	<link href="<?php echo base_url() ?>assets_F/img/brand/favicon.png" type="image/png">	
 
 </head>
 <body>
