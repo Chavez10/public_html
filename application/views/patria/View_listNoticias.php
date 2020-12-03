70a62ccd7f5d39ff06bf8c44146a050a7cd566ff
Lucifer <fer.reyes151719@gmail.com>
2020-12-02 19:05:56 -0600

    Fix Noticias

--- application/views/patria/View_listNoticias.php
+++ application/views/patria/View_listNoticias.php
@@ -1,3 +1,4 @@
+<!-- section -->
 <div class="section padding_layout_1 service_list">
     <div class="container">
         <div class="row">
@@ -8,104 +9,96 @@
                     </div>
                 </div>
             </div>
-            <!-- section -->
-            <div class="section padding_layout_1 service_list">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-01.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Data recovery</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-02.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Computer repair</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-03.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Network solutions</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-04.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Data recovery</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-05.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Computer repair</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-06.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Network solutions</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-07.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Data recovery</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-08.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Computer repair</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4 service_blog margin_bottom_50">
-                            <div class="full">
-                                <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-01.jpg" alt="#" /> </div>
-                                <div class="service_cont">
-                                    <h3 class="service_head">Network solutions</h3>
-                                    <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                                    <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
-                                </div>
-                            </div>
-                        </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-01.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Data recovery</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
                     </div>
                 </div>
             </div>
-            <!-- end section -->
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-02.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Computer repair</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-03.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Network solutions</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-04.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Data recovery</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-05.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Computer repair</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-06.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Network solutions</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-07.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Data recovery</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-08.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Computer repair</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 service_blog margin_bottom_50">
+                <div class="full">
+                    <div class="service_img"> <img class="img-responsive" src="assets_F/images/it_service/post-01.jpg" alt="#" /> </div>
+                    <div class="service_cont">
+                        <h3 class="service_head">Network solutions</h3>
+                        <p>Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+                        <div class="bt_cont"> <a class="btn sqaure_bt" href="it_service_detail.html">View Service</a> </div>
+                    </div>
+                </div>
+            </div>
             <div class="center">
                 <ul class="pagination style_1">
                     <li><a href="#"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>
@@ -116,3 +109,4 @@
             </div>
         </div>
     </div>
+</div>
