e6fec6d2decc0129a96efe769be8965794d6dc51
Gilberto98 <gilberto.ponce14@gmail.com>
2021-01-19 16:15:58 -0600

    c

---
+++ application/views/patria/editorial.php
@@ -0,0 +1,78 @@
+<!-- inner page banner -->
+<div id="inner_banner" class="section inner_banner_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="full">
+                    <div class="title-holder">
+                        <div class="title-holder-cell text-left">
+                            <h1 class="page-title">Blog Detail</h1>
+                            <ol class="breadcrumb">
+                                <li><a href="index.html">Home</a></li>
+                                <li class="active">Blog Detail</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end inner page banner -->
+<!-- <div class="section padding_layout_1 service_list">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="full" style="margin-top: 15px;">
+                            <h2>Need file recovery</h2>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                                quasi architecto beatae vitae dicta sunt explicabo.. </p>
+                            <p><a class="btn main_bt" href="#">Read More</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+<div class="section padding_layout_1 blog_list">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 pull-right">
+                <div class="full">
+                    <div class="blog_section">                        
+                        <div class="blog_feature_cantant">
+                            <p class="blog_head">Blogpost With Image</p>
+                            <div class="post_info">
+                                <ul>
+                                    <li><i class="fa fa-user" aria-hidden="true"></i> Marketing</li>
+                                    <li><i class="fa fa-comment" aria-hidden="true"></i> 5</li>
+                                    <li><i class="fa fa-calendar" aria-hidden="true"></i> 12 Aug 2017</li>
+                                </ul>
+                            </div>
+                            <p>Consectetur, assumenda provident lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae laboriosam sunt hic perspiciatis,
+                                asperiores mollitia excepturi voluptatibus sequi nostrum ipsam veniam omnis nihil! A ea maiores corporis. Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                            <div class="bottom_info">
+                                <div class="pull-left"><a class="btn sqaure_bt" href="it_blog_detail.html">Read More<i class="fa fa-angle-right"></i></a></div>
+                                <div class="pull-right">
+                                    <div class="shr">Share: </div>
+                                    <div class="social_icon">
+                                        <ul>
+                                            <li class="fb"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                            <li class="twi"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                            <li class="gp"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                            <li class="pint"><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>s
